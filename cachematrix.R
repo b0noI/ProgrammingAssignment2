6fec40a140ab2890e369bc842d1fb55b5324579b
Viacheslav V. Kovalevskyi <Viacheslav@b0noI.com>
2014-04-11 20:57:17 +0100

    creating methods makeCacheMatrix/cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## functions calculating solve for matrix with caching. 
+## If solve already calculated for matrix it will be returned from cache
+## If user set's new matrix, cache will be dropped
 
+
+## makeCacheMatrix - create object with 4 fumctions:
+## set - set matrix and delete cached solve
+## get - get matrix
+## setsolve - save solve to cache
+## getsolve - load solve from cache
+## input of function - matrix()
 makeCacheMatrix <- function(x = matrix()) {
-
+  solve <- NULL
+  set <- function(y) {
+    matrix <<- y
+    solve <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(y) solve <<- y
+  getsolve <- function() solve
+  list(set = set, 
+       get = get, 
+       setsolve = setsolve, 
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve will try to read solve for matrix from cache and if fails it will calculate solve and save it to cache
+## if it's read solve from cach it's print: "getting cached data"
+## input - tuple that can be created with makeCacheMatrix function
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
